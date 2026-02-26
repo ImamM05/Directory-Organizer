5343cc4c2c72e801cd8d8994ead7676a93899096
ImamM <imammishkat2@gmail.com>
2026-02-26 11:58:49 -0500

    more stuff

--- Assignment_1.c
+++ Assignment_1.c
@@ -6,58 +6,50 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define BUFFER_AMT 10000
+#define EXT_BUFF 100
+#define BTR 4
 #define CHAR_LENGTH 100
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
+    char buf[EXT_BUFF];
+    ssize_t byteRead;
+    char** extenList;
+
+    if (argc < 2) {
         printf("Error: Missing filepath and txt file, ex: %s <filepath> <filename>\n", argv[0]);
         return -1;
     }
 
     int fd = open(argv[1], O_RDONLY);
+
     if (fd == -1) {
         perror("Error opening file");
         return -1;
     }
-
-    char* buf = (char*)calloc(BUFFER_AMT, sizeof(char));
     if (buf == NULL) {
         perror("Memory allocation failed.");
         return -1;
     }
 
-    ssize_t byteRead = read(fd, buf, BUFFER_AMT);
-    if (byteRead == -1){
-        perror("Error reading file");
-        return -1;
-    }
-    char token[CHAR_LENGTH];
-    int j = 0;
-
-    // process bytes read from file
-    for (unsigned int i = 0; i < byteRead; i++) {
-        if (j < CHAR_LENGTH - 1 && isalnum(buf[i])) {
-            token[j++] = buf[i];
-        } else if (j==CHAR_LENGTH -1) {
-            perror("Error token is longer than memory reserved for characters.")
-        } else {
-            if (j > 0) {
-                token[j] = '\0';
-                printf("%s\n", token);
-                j = 0;
-            }
+    extenList = malloc(20 * sizeof(*extenList));
+    while ((byteRead = read(fd, buf, BTR)) > 0) {
+        for (unsigned int i = 0; i < byteRead; i++) {
+            if 
         }
     }
-    if (j > 0) {
-        token[j] = '\0';
-        printf("%s\n", token);
-        j = 0;
+    if (byteRead == -1) {
+        perror("Error reading file.")
     }
 
+   
 
-    free(buf);
+
+    free(extenList);
     close(fd);
     return 0;
     
+}
+
+void addToList(const char** const list, const char* extension) {
+
 }
